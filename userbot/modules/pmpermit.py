a97e5d26b38acc3d1cb06ad53601be35213668c2
Rajesh Rajput <40692774+RajeshRajout@users.noreply.github.com>
2020-06-09 08:12:28 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -19,7 +19,8 @@ from userbot.events import register
 UNAPPROVED_MSG = ("`Welcome, You are a wonderful person with a wonderful view of life.`\n\n"
                   "`Your companionship is always an opportunity But Sed This line is not for you.`"
                   "`So just tell why you are here ?`\n\n"
-                  "`Your message will be responded when I'm free,Don't sit here whole day coz there are lots of pending messages😶‚`")
+                  "`Your message will be responded when I'm free,Don't sit here whole day coz there are lots of pending messages😶‚`"
+                 "`In the mean time, check our YouTube Channel https://bit.ly/37oy3dL or Join us on Telegram https://t.me/technobuzznet`")
 # =================================================================
 
 
